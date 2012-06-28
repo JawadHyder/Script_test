ad92f1b093ebb81596a90bff8269e08e9dabe6fc
JawadHyder <jawadhyder15@gmail.com>
2012-06-28 19:46:22 +0600

    Update master

--- b.js
+++ b.js
@@ -1,3 +1,3 @@
 $('<div></div>',{
-  style:"background-color:#000; width:100%; height:100%; top:0; left:0; position:fixed; z-index:100;"
+  style:"background-color:#000; width:100%; height:100%; top:0; left:0; position:fixed; z-index:100; opacity:0.3;"
 }).appendTo('body');
